530f70afc0e61fad08d1ffcf341a210e41202dc9
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-11-14 18:32:54 +0000

    Create question class

--- main.rb
+++ main.rb
@@ -1,22 +1,36 @@
 require './player'
+require './question'
 
-#Player test code
-player1 = Player.new("Player 1")
+####################
+# Player test code #
+####################
+# player1 = Player.new("Player 1")
 
-# 3 Lives
-puts player1.lives
-puts player1.win
+# # 3 Lives
+# puts player1.lives
+# puts player1.win
 
-# 2 Lives
-player1.take_life
-puts player1.lives
-puts player1.win
+# # 2 Lives
+# player1.take_life
+# puts player1.lives
+# puts player1.win
 
-# 1 Life
-player1.take_life
-puts player1.lives
-puts player1.win
+# # 1 Life
+# player1.take_life
+# puts player1.lives
+# puts player1.win
 
-# 0 Lives
-player1.take_life
-puts player1.lives
+# # 0 Lives
+# player1.take_life
+# puts player1.lives
+
+######################
+# Question Test Code #
+######################
+question = Question.new
+puts question.question
+puts question.answer
+
+question = Question.new
+puts question.question
+puts question.answer

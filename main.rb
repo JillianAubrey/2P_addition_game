3425f5ef1b7169ae39406c709213b06c35c1fb6f
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-11-15 03:20:40 +0000

    Remove commented code

--- main.rb
+++ main.rb
@@ -3,47 +3,5 @@ require './question'
 require './turn'
 require './game'
 
-# Start a new game
+# Starts a new game
 Game.new.run
-
-
-####################
-# Player test code #
-####################
-# player1 = Player.new(1)
-
-# # 3 Lives
-# puts player1.lives
-# puts player1.win
-
-# # 2 Lives
-# player1.take_life
-# puts player1.lives
-# puts player1.win
-
-# # 1 Life
-# player1.take_life
-# puts player1.lives
-# puts player1.win
-
-# # 0 Lives
-# player1.take_life
-# puts player1.lives
-
-######################
-# Question Test Code #
-######################
-# question = Question.new
-# puts question.question
-# puts question.answer
-
-# question = Question.new
-# puts question.question
-# puts question.answer
-
-##################
-# Turn Test Code #
-##################
-# player = Player.new(1)
-# Turn.new(player).run
-# puts player.lives
